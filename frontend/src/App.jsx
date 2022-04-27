38c67e03394e68d4c6767b1964bcb7c07dc6233e
RaphaelNdonga <nkibi53@gmail.com>
2022-04-27 12:14:01 +0300

    changed images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 import SelectCharacter from './Components/SelectCharacter';
 import Arena from './Components/Arena'
-import {CONTRACT_ADDRESS, transformCharacterData} from './constants.jsx';
+import { CONTRACT_ADDRESS, transformCharacterData } from './constants.jsx';
 import myEpicGame from './utils/MyEpicGame.json';
-import {ethers} from 'ethers';
+import { ethers } from 'ethers';
 import LoadingIndicator from './Components/LoadingIndicator';
+import avengersIcon from "./favicon.ico";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'ndonga_raphael';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -18,91 +19,91 @@ const App = () => {
   const [characterNFT, setCharacterNFT] = useState(null);
 
   const [isLoading, setIsLoading] = useState(false);
-  
-	const checkIfWalletIsConnected = async() => {
-		try{
+
+  const checkIfWalletIsConnected = async () => {
+    try {
       const { ethereum } = window;
-  		if (!ethereum) {
-  			console.log('Ensure you have metamask');
+      if (!ethereum) {
+        console.log('Ensure you have metamask');
         setIsLoading(false);
-  			return;
-  		} else {
-  			console.log('Metamask present');
-        const accounts = await ethereum.request({method:'eth_accounts'});
-        if(accounts.length !== 0){
+        return;
+      } else {
+        console.log('Metamask present');
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        if (accounts.length !== 0) {
           const account = accounts[0];
           console.log("Connected account found! ", account);
           setCurrentAccount(account);
-        }else{
+        } else {
           console.log("No account connected");
         }
       }
-      
-		}catch(error){
-      console.log("Error while checking wallet connection: ",error);
+
+    } catch (error) {
+      console.log("Error while checking wallet connection: ", error);
     }
     setIsLoading(false);
-	};
+  };
 
-  const renderContent = ()=>{
-    if(isLoading){
+  const renderContent = () => {
+    if (isLoading) {
       return <LoadingIndicator />;
     }
-    
-    if(!currentAccount){
-      return(
+
+    if (!currentAccount) {
+      return (
         <div className="connect-wallet-container">
-						<img
-							src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-							alt="Monty Python Gif"
-						/>
-            <button className="cta-button connect-wallet-button" onClick={connectWalletAction}>Connect Wallet to Get Started</button>
-					</div>
+          <img
+            src="https://64.media.tumblr.com/c08954521ac6edf186be9a4e3ed118c1/dc979cf453fc7c40-a5/s400x600/82c0058d1b1ecfe707571e366ca9145c781c7f47.gifv"
+            alt="Monty Python Gif"
+          />
+          <button className="cta-button connect-wallet-button" onClick={connectWalletAction}>Connect Wallet to Get Started</button>
+        </div>
       )
-    }else if(currentAccount && !characterNFT){
-      return(
-        <SelectCharacter setCharacterNFT={setCharacterNFT}/>
+    } else if (currentAccount && !characterNFT) {
+      return (
+        <SelectCharacter setCharacterNFT={setCharacterNFT} />
       )
-    }else if(currentAccount && characterNFT){
-      return(
-        <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT}/>
+    } else if (currentAccount && characterNFT) {
+      return (
+        <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT} />
       )
     }
   };
 
-  const connectWalletAction = async()=>{
+  const connectWalletAction = async () => {
     try {
-      const {ethereum} = window;
-      if(!ethereum){
+      const { ethereum } = window;
+      if (!ethereum) {
         alert("Get metamask first");
       }
-      const accounts = await ethereum.request({method: 'eth_requestAccounts'});
-      console.log("Connected to : ",accounts[0]);
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Connected to : ", accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
-      console.log("Error while connecting wallet: ",error);
+      console.log("Error while connecting wallet: ", error);
     }
   }
 
-  const checkNetworkVersion = async()=>{
-    try{
-      if(window.ethereum.networkVersion !== '4'){
+  const checkNetworkVersion = async () => {
+    try {
+      if (window.ethereum.networkVersion !== '4') {
         alert("Please connect to the rinkeby network");
       }
-    }catch(error){
-      console.log("Error checking network version: ",error);
+    } catch (error) {
+      console.log("Error checking network version: ", error);
     }
   }
-  
 
-	useEffect(() => {
+
+  useEffect(() => {
     setIsLoading(true);
-		checkIfWalletIsConnected();
+    checkIfWalletIsConnected();
     checkNetworkVersion();
-	}, []);
+  }, []);
 
-  useEffect(()=>{
-    const fetchNFTMetadata = async()=>{
+  useEffect(() => {
+    const fetchNFTMetadata = async () => {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const myEpicGameContract = new ethers.Contract(
@@ -111,39 +112,40 @@ const App = () => {
         signer
       )
       const txn = await myEpicGameContract.checkIfUserHasNFT();
-      if(txn.name){
-        console.log("This user has NFT: ",txn.name)
+      if (txn.name) {
+        console.log("This user has NFT: ", txn.name)
         setCharacterNFT(transformCharacterData(txn));
-      }else{
+      } else {
         console.log("This user does not have an NFT");
       }
-    setIsLoading(false);
+      setIsLoading(false);
     }
 
-    if(currentAccount){
+    if (currentAccount) {
       fetchNFTMetadata();
     }
-  },[currentAccount])
-	return (
-		<div className="App">
-			<div className="container">
-				<div className="header-container">
-					<p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-					<p className="sub-text">Team up to protect the Metaverse!</p>
+  }, [currentAccount])
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <img alt="Avengers Logo" className="twitter-logo" src={avengersIcon} />
+          <p className="header gradient-text"> Avengers Endgame </p>
+          <p className="sub-text">Team up to protect the Universe from Thanos!</p>
           {renderContent()}
-				</div>
-				<div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
-				</div>
-			</div>
-		</div>
-	);
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
