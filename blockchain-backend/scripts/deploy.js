e2a8b057f5a443408ca775dbf9b44e2767b00ed7
RaphaelNdonga <nkibi53@gmail.com>
2022-04-28 08:57:23 +0300

    json files changed

--- blockchain-backend/scripts/deploy.js
+++ blockchain-backend/scripts/deploy.js
@@ -3,7 +3,7 @@ const main = async () => {
     const gameContract = await gameContractFactory.deploy(
         ["SpiderMan", "DrStrange", "IronMan"],       // Names
         ["https://www.nationalworld.com/webimg/QVNIMTIzNzg5MTAz.jpg", // Images
-            "https://static.wikia.nocookie.net/marveldatabase/images/6/67/Stephen_Strange_%28Earth-199999%29_from_Spider-Man_No_Way_Home_Promo_001.jpg/revision/latest?cb=20211216172929",
+            "https://www.cnet.com/a/img/resize/41e73c5ac49a7b175534bcd57d6b46c4776472dc/2016/10/28/3809e66e-d3fe-46bb-963a-705d88f5a902/doctor-strange6.jpg?auto=webp",
             "https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg"
         ],
         [100, 200, 300],                    // HP values
@@ -14,10 +14,32 @@ const main = async () => {
         50);
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
+    storeContractData(gameContract, "MyEpicGame");
 
     console.log("Done deploying and minting!");
 
 };
+
+function storeContractData(contract, contractName) {
+    const fs = require("fs");
+    const contractsDir = __dirname + "/../../frontend/src/utils";
+
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
+
+    fs.writeFileSync(
+        contractsDir + `/${contractName}-address.json`,
+        JSON.stringify({ Address: contract.address }, undefined, 2)
+    );
+
+    const contractArtifact = artifacts.readArtifactSync(contractName);
+
+    fs.writeFileSync(
+        contractsDir + `/${contractName}.json`,
+        JSON.stringify(contractArtifact, null, 2)
+    );
+}
 
 const runMain = async () => {
     try {
